d9af3e380eedd0b8d60d228566e919df0aebd32f
Justin <lego.man.123@hotmail.com>
2019-02-16 10:06:17 -0700

    Feature Matching class added which will process the video before tracking.

---
+++ src/video_process/feat_match.py
@@ -0,0 +1,22 @@
+"""
+This is where feature matching happens. 
+The video will be inputted, and the video will be processed 
+and output a stabalized videofile and a stitched image.
+This process will be saved and loaded for each video, processing it ONCE.
+"""
+
+
+def stabalize():
+    """
+    This process will use Feature Matching from two frames and ?Average? 
+    the difference between the two... who knows.
+    """
+    pass
+
+
+def stitch():
+    """
+    This process will use Feature Matching from the difference in frames 
+    across the whole video and stitch them together into one large image
+    """
+    pass
