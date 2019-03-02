20d5acc3219b312f5634e194c11cf9dbae32c6c2
Justin <lego.man.123@hotmail.com>
2019-03-01 22:03:44 -0700

    Stitched image - finding keypoints between all the images

--- src/video_process/image.py
+++ src/video_process/image.py
@@ -7,13 +7,25 @@ import sys
 # from random import randrange
 
 class StitchImage():
+    """
+    Steps to stitching the images
+    1) Find Keypoints between images (SIFT/SURF/ORB?)
+    2) Compute the distances between every descriptor
+
+    3) Select the best matches to align (Best of 2? feature matcher)
+    - finds two best matches for each feature and leaves the best one only if the ratio between descriptor distances is greater than the threshold
+    4) Estimate Homography (Homography Estimator)
+    5) Warp the images (translate and rotate) to align
+        - NOTE this is where we will extract the top left of each frame for reference on the stitched image
+        locations of each frame (hopefully)
+    6) Stitch the images (Stitch and blend)
+    """
     def __init__(self):
         pass
 
     def stitch(self):
         """
         This function takes in the video
-        
         ----------------
         Stitcher methods
         ----------------
@@ -45,18 +57,19 @@ class StitchImage():
         """
         print("Stitching Frames...")
 
-        frames = self.collect_frames("./videos/GH010018_Trim.mp4")
-
+        frames = self.collect_frames("./videos/Cap2_2k_Trim.mp4")
+        keypoints, descriptor = self.find_KeyPoints(frames)
+        print(descriptor)
         # stitcher = cv2.createStitcherScans(True)
-        stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
-        status, pano = stitcher.stitch(frames)
+        # stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
+        # status, pano = stitcher.stitch(frames)
 
-        if status != cv2.Stitcher_OK:
-            print("Can't stitch images, error code = %d" % status)
-            sys.exit(-1)
+        # if status != cv2.Stitcher_OK:
+        #     print("Can't stitch images, error code = %d" % status)
+        #     sys.exit(-1)
         
         print("Stitching Successful.")
-        cv2.imwrite("stitched_linear.jpg", pano);
+        cv2.imwrite("stitched_linear.jpg", scan);
 
     def collect_frames(self, video_source):
         """
@@ -68,27 +81,40 @@ class StitchImage():
             raise ValueError("Unable to open video source", video_source)
         print("Collecting Frames...")
 
+        #setup cv2 capture from video
         cap = cv2.VideoCapture(video_source)
         frames = []
         frame_skip = 105
 
         while len(frames) < 12:
+            #set current frame to the next n-skipped frames
             cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + frame_skip)
             print(cap.get(cv2.CAP_PROP_POS_FRAMES))
+
+            #read the image from that skipped frame
             ret, frame = cap.read()
             if ret:
                 if cv2.waitKey(30) & 0xFF == ord('q'):
                     break
                 cv2.imshow('frame', frame)
-
+                #append the frames to be processed
                 frames.append(frame)
         
         return frames
     
-    def find_KeyPoints(self):
-        pass
+    def find_KeyPoints(self, frames):
+        #ORB: An efficient alternative to SIFT or SURF
+        #this creates the parameters to be used
+        orb = cv2.ORB_create()
+        for frame in frames:
+            # find the keypoints with ORB
+            keypoints = orb.detect(frame, None)
+            #compute the descriptors with ORB
+            keypoints, des = orb.compute(frame, keypoints)
+        return keypoints, des
     
     def match_Keypoints(self):
+        #cv2.estimateAffinePartial2D()
         pass
 
     def reference_frame(self, current_frame):
