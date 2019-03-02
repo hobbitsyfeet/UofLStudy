14b6aaf86a70fdb025340e26eff1ea55c33794f2
Justin <lego.man.123@hotmail.com>
2019-03-01 20:54:15 -0700

    Updated Opencv
    - opencv was updated to allow access to more image stitching functions.

--- src/video_process/image.py
+++ src/video_process/image.py
@@ -2,9 +2,10 @@
 from __future__ import print_function
 import cv2 as cv2
 import numpy as np
-import argparse
 import sys
- 
+# import matplotlib.pyplot as plt
+# from random import randrange
+
 class StitchImage():
     def __init__(self):
         pass
@@ -12,12 +13,42 @@ class StitchImage():
     def stitch(self):
         """
         This function takes in the video
+        
+        ----------------
+        Stitcher methods
+        ----------------
+
+        register resolution(0.6)
+        estimation resolution(0.1)
+        compsiting resolution (original resolution)
+        Panorama confidence threshold(1) = certain?
+        seam finder -> graphCutSeamFinder - detail COST COLOR?
+        Blender -> Multiband blender(false)
+        Feature finder (ORB)
+        Interpolation flags -> INTER_LINEAR
+
+        work scale = 1
+        seam scale = 1
+        seam work aspect = 1
+        warped image scale = 1
+
+        ---------------------
+        SCANS stitcher modes.
+        ---------------------
+        estimator -> AffineBasedEstimator
+        wave correction -> False
+        Feature matcher ->best of 2 nearest matcher(false,false)
+        Bundle adjuster -> BundleAdjusterAffinePartial
+        warper -> affine
+        exposureCompensator -> no compensation
+
         """
         print("Stitching Frames...")
 
         frames = self.collect_frames("./videos/GH010018_Trim.mp4")
 
-        stitcher = cv2.createStitcherScans(True)
+        # stitcher = cv2.createStitcherScans(True)
+        stitcher = cv2.Stitcher_create(cv2.Stitcher_SCANS)
         status, pano = stitcher.stitch(frames)
 
         if status != cv2.Stitcher_OK:
@@ -26,7 +57,6 @@ class StitchImage():
         
         print("Stitching Successful.")
         cv2.imwrite("stitched_linear.jpg", pano);
-
 
     def collect_frames(self, video_source):
         """
@@ -40,7 +70,7 @@ class StitchImage():
 
         cap = cv2.VideoCapture(video_source)
         frames = []
-        frame_skip = 100
+        frame_skip = 105
 
         while len(frames) < 12:
             cap.set(cv2.CAP_PROP_POS_FRAMES, cap.get(cv2.CAP_PROP_POS_FRAMES) + frame_skip)
@@ -50,9 +80,16 @@ class StitchImage():
                 if cv2.waitKey(30) & 0xFF == ord('q'):
                     break
                 cv2.imshow('frame', frame)
+
                 frames.append(frame)
         
         return frames
+    
+    def find_KeyPoints(self):
+        pass
+    
+    def match_Keypoints(self):
+        pass
 
     def reference_frame(self, current_frame):
         """
