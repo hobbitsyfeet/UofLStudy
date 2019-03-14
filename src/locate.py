b3b6d05ce19a5def65f695956fd4597f0c999951
Justin <lego.man.123@hotmail.com>
2019-03-14 17:19:05 -0600

    Points can be assigned on the image
    - gps coordinates are stored in assigned, where pixel values are paired with GPS values.
    - assigned points can be selected, added and deleted
    - a beautiful green colour is shown when the current point is selected

--- src/locate.py
+++ src/locate.py
@@ -14,16 +14,38 @@ class Locate():
         """
         The parent window is the program that originally calls it.
         """
+        self.window = parent
         # self.data = data
         self.coordinates = []
+        self.assigned = []
         self.data = None
         self.video_source = video_source
         self.vid_length = None
         self.img_processor = StitchImage()
+        self.process_image = None
 
         self.step = 100
         self.stitched_frames=10
+        self.delay = 15
         
+    def update(self):
+        self.id_coordinates.config(text=self.get_data_by_id(int(self.tkvar.get())))
+            # print(self.assigned[0][0])
+        display = self.process_image.copy()
+        # gray = cv2.cvtColor(display, cv2.COLOR_BGR2GRAY)
+        current_coord = self.get_data_by_id(int(self.tkvar.get()))
+        for points in range(len(self.assigned)):
+            colour = (255,0,0)
+            if current_coord == self.assigned[points][1]:
+                colour = (0,255,0)
+
+                
+            cv2.circle(display, tuple(self.assigned[points][0]), 10, colour, -1, cv2.LINE_AA)
+        self.photo = ImageTk.PhotoImage(image=Image.fromarray(display))
+        self.canvas.create_image(0,0,image=self.photo, anchor="nw")
+
+        self.window.after(self.delay, self.update)
+
     def add_coordinate(self, lon, lat):
         """
         We add coordinates based on longitude , latitude
@@ -58,20 +80,32 @@ class Locate():
         Uses the ID of the coordinates to assign them on the image
         This function should be called by clicking.
         """
-        print(str(event.x) +", " + str(event.y))
+
+        if len(self.assigned) == 0:
+            self.assigned.append( ( (event.x, event.y), (self.get_data_by_id(int(self.tkvar.get()))) ) )
+
+        else:
+            found = False
+            current_coord = self.get_data_by_id(int(self.tkvar.get()))
+            for points in range(len(self.assigned)):
+                
+                if current_coord == self.assigned[points][1]:
+                    found = True
+                    self.assigned[points] = ( ((event.x, event.y), current_coord) )
+                    break
+            if not found:
+                self.assigned.append( ( (event.x, event.y), (self.get_data_by_id(int(self.tkvar.get()))) ) )
+    
         return event.x, event.y
     
-    def get_coord_id(self):
-        pass
-    
-    def set_current_coord(self):
-        pass
-
-    def remove_assigned_coord(self, coord_id):
+    def remove_assigned_coord(self):
         """
         removes the location of an assigned coordinate
         """
-        pass
+        current_coord = self.get_data_by_id(int(self.tkvar.get()))
+        for points in range(len(self.assigned)):
+            if current_coord == self.assigned[points][1]:
+                self.assigned.pop(points)
 
 
     def start(self, finish_frame):
@@ -100,46 +134,44 @@ class Locate():
 
         
         h,w = scan.shape[:2]
-
+        self.process_image = scan
         self.data = self.load_coords()
 
-        # displayed in a separate, top-level window. Such windows usually have title bars, borders, and other “window decorations”
-        locate_window = tkinter.Toplevel()
-        locate_window.title("Stitch and Locate")
-
-        # option_panel=tkinter.Frame(locate_window,width=200, height=200)
+        # option_panel=tkinter.Frame(self.window,width=200, height=200)
         # option_panel.pack(side=tkinter.LEFT)
 
-        self.setup_dropdown(locate_window, self.data)
+        self.setup_dropdown(self.window, self.data)
         #create a frame inside the window which will contain the canvas
-        frame=tkinter.Frame(locate_window,width=w,height=h)
+        frame=tkinter.Frame(self.window,width=w,height=h)
         frame.pack(side=tkinter.LEFT)
 
         
         #Create a canvas, the area where the image is shown. Scroll region is the size of the image
-        canvas=tkinter.Canvas(frame,width=w,height=h, scrollregion=(0,0,w,h),cursor="crosshair")
+        self.canvas=tkinter.Canvas(frame,width=w,height=h, scrollregion=(0,0,w,h),cursor="crosshair")
 
         #create the structual scrollbars
         hbar=tkinter.Scrollbar(frame,orient=tkinter.HORIZONTAL)
         hbar.pack(side=tkinter.BOTTOM,fill=tkinter.X)
-        hbar.config(command=canvas.xview)
+        hbar.config(command=self.canvas.xview)
         vbar=tkinter.Scrollbar(frame,orient=tkinter.VERTICAL)
         vbar.pack(side=tkinter.RIGHT,fill=tkinter.Y)
-        vbar.config(command=canvas.yview)
+        vbar.config(command=self.canvas.yview)
 
         #link the scrollregion to the structural scrollbars
-        canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
-        canvas.pack(side=tkinter.LEFT, expand=True,fill=tkinter.BOTH)
+        self.canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
+        self.canvas.pack(side=tkinter.LEFT, expand=True,fill=tkinter.BOTH)
         
         #set the image inside of the region
-        photo = ImageTk.PhotoImage(image=Image.fromarray(scan))
+        self.photo = ImageTk.PhotoImage(image=Image.fromarray(self.process_image))
         #0,0 is the image location we anchor, anchor is how. If not anchored to nw, it sets the center of the image to top left corner 
-        canvas.create_image(0,0,image=photo, anchor="nw")
+        self.canvas.create_image(0,0,image=self.photo, anchor="nw")
         
-        canvas.bind("<Button-1>", self.assign_coord)
+        self.canvas.bind("<Button-1>", self.assign_coord)
 
         
-        locate_window.mainloop()
+        self.update()
+        self.window.mainloop()
+
     def setup_dropdown(self, window, data):
         print("Setting up dropdown")
         # Create a Tkinter variable
@@ -148,33 +180,28 @@ class Locate():
         
         #duplicate first index
         all_id.insert(0, all_id[0])
-        # for self.data
-        # self.choices = []
-        #add data to list
-        # for data_index in range(len(data)):
-            #Add data
-            # values = data.pop()
-            # #if it's the first, append it twice
-            # if data_index == 0:
-            #    self.choices.append(str(data[data_index])) 
-            # self.choices.append(str(data[data_index]))
-        #setup the menu
-        popup_menu = ttk.OptionMenu(window, self.tkvar, *all_id)
-        print(self.get_data_by_id(1))
-        popup_lable = tkinter.Label(window, text="GPS Coordinates")
+        self.tkvar.set(all_id[0])
 
+        #assign all id's to the option menu
+        popup_menu = ttk.OptionMenu(window, self.tkvar, *all_id)
+        popup_lable = tkinter.Label(window, text="GPS Coordinates")
+        self.id_coordinates = tkinter.Label(window, text=self.get_data_by_id(int(self.tkvar.get())))
+        remove = tkinter.Button(window, text="Remove", command=self.remove_assigned_coord)
+        #set the option menu
         popup_lable.pack(side=tkinter.TOP)
         popup_menu.pack(side=tkinter.TOP)
-        
+        self.id_coordinates.pack(side=tkinter.TOP)
+        remove.pack(side=tkinter.TOP)
         
 
     def get_data_by_id(self, id):
+        id_type = self.data.ID.dtype
+
         #get the row which the id matches
         id_loc = self.data.loc[self.data['ID'] == id]
-        print(id_loc)
-        #get the coordinates of that
-        x_coord = id_loc.loc[0]['X']
-        y_coord = id_loc.loc[0]['Y']
+        #get the coordinates of that id
+        x_coord = id_loc.iloc[0]['X']
+        y_coord = id_loc.iloc[0]['Y']
         return x_coord, y_coord
 
 
@@ -198,11 +225,15 @@ class Locate():
 
         status, scan = self.img_processor.stitch(frames)
         print("Stitching Complete.")
-
         return scan
 
 if __name__ == "__main__":
-        locate_tool = Locate(tkinter.Tk(), "./videos/GH010018.mp4")
+    
+        # displayed in a separate, top-level window. Such windows usually have title bars, borders, and other “window decorations”
+        locate_window = tkinter.Toplevel()
+        locate_window.title("Stitch and Locate")
+
+        locate_tool = Locate(locate_window, "./videos/GH010018.mp4")
         # cv2.imshow("FRAME", frame)
         #first is the frame itself the other is the frame number
         locate_tool.start(locate_tool.vid_length)
